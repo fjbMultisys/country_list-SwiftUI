bc7db6ebbccae023d8ba85b9337b2a49f1bc6382
Francis Jemuel Bergonia <fabergonia@multisyscorp.com>
2023-04-02 12:04:50 +0800

    Initial Commit

---
+++ CountryList/CountryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryListApp.swift
+//  CountryList
+//
+//  Created by MYS on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CountryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
