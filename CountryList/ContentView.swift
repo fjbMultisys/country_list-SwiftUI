23d3b7257bb7854268201c56f3fdb7382fbc4e95
Francis Jemuel Bergonia <fabergonia@multisyscorp.com>
2023-04-02 12:07:15 +0800

    app complete

--- CountryList/ContentView.swift
+++ CountryList/ContentView.swift
@@ -8,19 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    let countries = ["United States", "Canada", "Mexico", "Brazil", "Argentina"]
+    let capitals = ["Washington D.C.", "Ottawa", "Mexico City", "Brasília", "Buenos Aires"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(0..<countries.count) { index in
+                NavigationLink(destination: CountryDetailView(country: countries[index], capital: capitals[index])) {
+                    Text(countries[index])
+                }
+            }
+            .navigationTitle("Countries")
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+struct CountryDetailView: View {
+    let country: String
+    let capital: String
+
+    var body: some View {
+        VStack {
+            Image(country)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+
+            Text(country)
+                .font(.largeTitle)
+                .bold()
+
+            Text("Capital: \(capital)")
+                .font(.title2)
+                .foregroundColor(.gray)
+        }
+        .navigationTitle(country)
+    }
+}
+
